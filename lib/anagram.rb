f4464a95710df3017e15b1b08d5806a3cb6ee63d
Dimitri Gedda <gratitudeadore@fastmail.com>
2019-12-16 09:35:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(string)
+    @string = string
+  end
+  
+  def match(array)
+    word = []
+    array.each {|item|                 
+      count = 0
+      if item.length == @string.length
+        x = @string.split('').each {|char|
+        if item.include?(char)
+          count +=1
+        end
+      }
+    end
+          if count == item.length
+            word << item
+          end
+    }
+    p word
+        end
+ 
+   
+    
+    
+  end
