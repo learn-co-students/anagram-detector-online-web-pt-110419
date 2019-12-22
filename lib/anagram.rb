117241f253ac8fd66ad641bdd54b050ec49d50bd
Angelica  Thompson <i.thompson89@outlook.com>
2019-12-22 17:32:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram 
+  
+ attr_accessor :anagram
+ 
+ def initialize 
+   @anagram = anagram
+ end 
+ 
+ angram.match(angrams) {|x| angrams}
+ #detect if there is no match in the Array
+ #str.match(pat) {|m| ...}
+end 
