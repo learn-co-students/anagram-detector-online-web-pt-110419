8acebd37a627f45924c2ca01174e79f57e96f040
Angelica  Thompson <i.thompson89@outlook.com>
2019-12-22 20:11:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,11 +2,14 @@ class Anagram
   
  attr_accessor :anagram
  
- def initialize 
+ def initialize(anagram) 
    @anagram = anagram
  end 
- 
+
+def anagram 
  angram.match(angrams) {|x| angrams}
  #detect if there is no match in the Array
  #str.match(pat) {|m| ...}
+ end 
+ 
 end 
